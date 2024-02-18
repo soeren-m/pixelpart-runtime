1028ce5be01ea64802d4cf35ca5d3a045d61b836
soeren-m <moellers.soeren@gmail.com>
2024-02-18 11:57:29 +0100

    WIP 1

--- project/PostProcessingEffectCollection.cpp
+++ project/PostProcessingEffectCollection.cpp
@@ -1,32 +1,19 @@
 #include "PostProcessingEffectCollection.h"
-#include "../common/StringUtil.h"
 #include "../common/JsonUtil.h"
 
 namespace pixelpart {
 PostProcessingEffectCollection::PostProcessingEffectCollection() {
 
 }
-PostProcessingEffectCollection::PostProcessingEffectCollection(const std::vector<PostProcessingEffectType>& effectTypes, const std::string& shTemplate) :
-	effects(effectTypes), shaderTemplate(shTemplate) {
+PostProcessingEffectCollection::PostProcessingEffectCollection(const std::vector<PostProcessingEffectType>& effectTypes) :
+	effects(effectTypes) {
 
-}
-
-std::string PostProcessingEffectCollection::build(const std::string& typeName) const {
-	const PostProcessingEffectType* effectType = getEffectType(typeName);
-	if(effectType == nullptr) {
-		return std::string();
-	}
-
-	std::string code = shaderTemplate;
-	code = replace(code, effectType->code, "{main}");
-
-	return code;
 }
 
 const PostProcessingEffectType* PostProcessingEffectCollection::getEffectType(const std::string& typeName) const {
-	for(std::size_t i = 0u; i < effects.size(); i++) {
-		if(effects[i].name == typeName) {
-			return &(effects.at(i));
+	for(const PostProcessingEffectType& effectType : effects) {
+		if(effectType.name == typeName) {
+			return &effectType;
 		}
 	}
 
@@ -36,23 +23,15 @@ const std::vector<PostProcessingEffectType>& PostProcessingEffectCollection::get
 	return effects;
 }
 
-const std::string& PostProcessingEffectCollection::getShaderTemplate() const {
-	return shaderTemplate;
-}
-
 void to_json(nlohmann::ordered_json& j, const PostProcessingEffectCollection& effectCollection) {
 	j = nlohmann::ordered_json{
-		{ "effects", effectCollection.getEffectTypes() },
-		{ "shader_template", effectCollection.getShaderTemplate() }
+		{ "effects", effectCollection.getEffectTypes() }
 	};
 }
 void from_json(const nlohmann::ordered_json& j, PostProcessingEffectCollection& effectCollection) {
 	std::vector<PostProcessingEffectType> effects;
 	fromJson(effects, j, "effects");
 
-	std::string shaderTemplate;
-	fromJson(shaderTemplate, j, "shader_template");
-
-	effectCollection = PostProcessingEffectCollection(effects, shaderTemplate);
+	effectCollection = PostProcessingEffectCollection(effects);
 }
 }
