90a194cbe3d01127c3892ae1d421dc840ffcaec1
soeren-m <moellers.soeren@gmail.com>
2024-02-27 17:00:15 +0100

    Simplify post-processing shader generation

--- project/PostProcessingEffectCollection.h
+++ project/PostProcessingEffectCollection.h
@@ -6,18 +6,13 @@ namespace pixelpart {
 class PostProcessingEffectCollection {
 public:
 	PostProcessingEffectCollection();
-	PostProcessingEffectCollection(const std::vector<PostProcessingEffectType>& effectTypes, const std::string& shTemplate);
-
-	std::string build(const std::string& typeName) const;
+	PostProcessingEffectCollection(const std::vector<PostProcessingEffectType>& effectTypes);
 
 	const PostProcessingEffectType* getEffectType(const std::string& typeName) const;
 	const std::vector<PostProcessingEffectType>& getEffectTypes() const;
 
-	const std::string& getShaderTemplate() const;
-
 private:
 	std::vector<PostProcessingEffectType> effects;
-	std::string shaderTemplate;
 };
 
 void to_json(nlohmann::ordered_json& j, const PostProcessingEffectCollection& effectCollection);
