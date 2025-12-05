e2c547ff3e387300776e53055dfd1672dbb41eaa
soeren-m <moellers.soeren@gmail.com>
2025-12-05 10:13:54 +0100

    Fix whitespace formatting

--- vertex/VertexFormatException.cpp
+++ vertex/VertexFormatException.cpp
@@ -2,15 +2,15 @@
 
 namespace pixelpart {
 VertexFormatException::VertexFormatException(const char* msg, std::int32_t attribIndex) :
-    std::runtime_error(msg), exceptionAttributeIndex(attribIndex) {
+	std::runtime_error(msg), exceptionAttributeIndex(attribIndex) {
 
 }
 VertexFormatException::VertexFormatException(const std::string& msg, std::int32_t attribIndex) :
-    std::runtime_error(msg), exceptionAttributeIndex(attribIndex) {
+	std::runtime_error(msg), exceptionAttributeIndex(attribIndex) {
 
 }
 
 std::int32_t VertexFormatException::attributeIndex() const {
-    return exceptionAttributeIndex;
+	return exceptionAttributeIndex;
 }
 }
